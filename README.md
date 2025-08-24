5842620721070797070670e4f249a94264fc85d0
Andrew Hahn <andrewstevenhahn@gmail.com>
2025-08-23 19:41:30 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ticas_rural-ed-explorer
+Web app for exploring and exporting demographic and geographic data about rural higher education in the U.S.
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // use your template
+      filename: 'index.html',          // output in dist/
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    hot: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
