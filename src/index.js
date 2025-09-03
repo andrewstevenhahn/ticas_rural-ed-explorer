152b41a4bde777e06ff98cc2f13afe998d87310b
Andrew Hahn <andrewstevenhahn@gmail.com>
2025-09-03 01:47:53 -0500

    Added map controls

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-// import bootstrap
+// import assets, libraries and custom styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import './styles.css';
 import logo from './assets/Books-UC-ORANGE.png';
-import czData from "./data/CZ.geojson";
 
+// import geojson data
+import countyData from "./data/county_simp.geojson";
+import czData from "./data/cz20_simp_num.geojson";
+import instData from "./data/institution.geojson";
+
+// set logo from asset
 const logoImg = document.querySelector('.navbar-brand img');
 logoImg.src = logo;
 
-// Initalize map
+// Initalize map with open source tiles 
 const map = new maplibregl.Map({
   container: "map",
   style: {
@@ -34,27 +39,65 @@ const map = new maplibregl.Map({
       }
     ]
   },
-  center: [-95.5795, 39.8283], // New York City
-  zoom: 3
+  center: [-95.5795, 39.8283],
+  zoom: 3,
+  cooperativeGestures: true
 });
 
-
+//handle events
 map.on("load", () => {
-  map.addSource("states", {
+
+  // add source data
+
+  map.addSource("cz20", { //commuting zones
     type: "geojson",
     data: czData
   });
 
-  // Add a line layer to render state outlines
+  map.addSource("county", { // counties
+    type: "geojson",
+    data: countyData
+  });
+
+  map.addSource("institution", { // institutions
+    type: "geojson",
+    data: instData
+  });
+
+  // render commuting zones
   map.addLayer({
-    id: "state-borders",
-    type: "line",
-    source: "states",
+    id: "cz20",
+    type: "fill",
+    source: "cz20",
     paint: {
-      "line-color": "#0000FF", // blue outlines
-      "line-width": 2
+      'fill-color': [
+        'interpolate', ['linear'], ['get', 'pct_rural'],
+        0, '#ffffcc',
+        0.2, '#a1dab4',
+        0.4, '#41b6c4',
+        0.6, '#2c7fb8',
+        0.8, '#253494'
+      ],
+      'fill-opacity': 0.7
     }
   });
+
+  // render institutions
+  // map.addLayer({
+  //   id:"institution",
+  //   type: "circle",
+  //   source: "institution",
+  //   paint: {
+  //     "circle-color": [
+  //     'match', ['get', 'level'],
+  //     'Public', '#e41a1c',
+  //     'Private non-profit', '#377eb8',
+  //     '#999999' // default
+  //   ],
+  //     "circle-radius": 4
+  //   }
+  // });
+
 });
 
 
@@ -64,8 +107,46 @@ map.addControl(new maplibregl.NavigationControl());
 // Add fullscreen control
 map.addControl(new maplibregl.FullscreenControl(), "top-right");
 
+// show loading animation until all map layers have loaded
 map.once("idle", () => {
   document.getElementById("spinner").classList.add("hidden");
 });
 
-console.log("Success")
+
+// Create a popup, but don't add it to the map yet
+const popup = new maplibregl.Popup({
+  closeButton: false,
+  closeOnClick: false
+});
+
+let currentFeatureId = undefined;
+
+map.on('mousemove', 'cz20', (e) => {
+  if (!e.features || e.features.length === 0) return;
+  const feature = e.features[0];
+
+  // Use feature.id to detect changes instead of coordinates
+  // if (currentFeatureId !== feature.properties.CZ20) {
+  currentFeatureId = feature.properties.CZ20;
+
+  // Change the cursor
+  map.getCanvas().style.cursor = 'pointer';
+
+  const description = 
+  '<div class="map-tooltip">'+
+  '<h7><strong>Commuting Zone ' + feature.properties.CZ20 + '</strong></h7>' +
+  '<p>Population 2020: ' + feature.properties.pop2020.toLocaleString()+'</p>' +
+  '<p>Percent Rural: ' + (feature.properties.pct_rural *100).toFixed(0) +'%</p>' +
+  '<p><em>Click for more details</em></p>'+
+  '</div>'
+
+  // Use the mouse position instead of polygon coordinates
+  popup.setLngLat(e.lngLat).setHTML(description).addTo(map);
+  
+});
+
+map.on('mouseleave', 'cz20', () => {
+  currentFeatureId = undefined;
+  map.getCanvas().style.cursor = '';
+  popup.remove();
+});
